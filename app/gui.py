1010be010f05bf304119e04034e08f4309160b00
tochicool <tochicool@gmail.com>
2016-03-13 18:00:55 +0000

    Added splash screen

--- app/gui.py
+++ app/gui.py
@@ -1,43 +1,56 @@
-from tkinter import *
-from tkinter import ttk
 import threading
 import time
 
-class splash(threading.Thread):
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
 
+class splash(threading.Thread):
+    setUp = False
     def __init__(self):
         threading.Thread.__init__(self)
         self.start()
 
-    def callback(self):
+        while not self.setUp:
+            pass
+
+    def close(self):
         self.root.quit()
 
     def run(self):
         self.root = Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.callback)
-        self.root.title("Splash Screen")
-        frame = ttk.Frame(self.root, padding="3")
-        #frame.grid(column=0, row=0, sticky=(N, W, E, S))
-        self.root.grid_columnconfigure(3, weight=1)
-        self.root.grid_rowconfigure(3,weight=1)
-
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
+        self.root.geometry('%dx%d+%d+%d' % (w//2, h//2, w//4, h//4))
         self.root.wm_attributes('-topmost', True)
-        self.root.wm_attributes('-fullscreen', True)
-        #root.wm_attributes('-alpha', 0.5)
 
-        self.progressBar = ttk.Progressbar(self.root, orient=HORIZONTAL, length=400, mode='determinate')
-        self.progressBar.grid(column = 3, row = 3)
+        load = Image.open('assets/images/splash.png')
+        ratio = (w//2)/load.width
+        load = load.resize((w//2, int(load.height*ratio)), Image.ANTIALIAS)
+        self.root.geometry('%dx%d+%d+%d' % (w//2, load.height, w//4, h//4))
+        render = ImageTk.PhotoImage(load)
+        image = ttk.Label(self.root, image=render)
 
+        self.progressBar = ttk.Progressbar(self.root, orient=HORIZONTAL, length=w, mode='determinate')
+        self.progressBar.pack(expand = True, side = BOTTOM, fill = BOTH)
+        print(self.progressBar['value'])
+        image.pack(expand = True, side = TOP, fill = BOTH)
+
+        self.root.overrideredirect(True)
+        self.setUp = True
         self.root.mainloop()
 
-splashScreen = splash()
+    def fillBar(self):
+        for i in range(101):
+            self.progressBar['value'] = i
+            time.sleep(0.01)
 
-import random
-while splashScreen.progressBar['value'] < 100:
-    time.sleep(0.01)
-    splashScreen.progressBar['value'] += random.random()*5
+if __name__ == '__main__':
+    splashScreen = splash()
+    splashScreen.fillBar()
 
-#splashScreen.overrideredirect(True)
+
 
 
 
---
+++ newt.py
@@ -0,0 +1,7 @@
+import app.gui as gui
+
+splashScreen = gui.splash()
+splashScreen.fillBar()
+splashScreen.close()
+
+
